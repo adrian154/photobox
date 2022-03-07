bb62f0fb02f2fcd0b0da296efb7cb6fc2a03fad9
adrian154 <94568a@gmail.com>
2022-03-07 00:14:56 -0800

    experimenting with alternative photogrid

--- static/scripts/photogrid.js
+++ static/scripts/photogrid.js
@@ -2,14 +2,6 @@ class PhotoGrid {
 
     constructor() {
         this.grid = document.getElementById("photogrid");
-        this.curRow = this.newRow();
-    }
-
-    newRow() {
-        const row = document.createElement("div");
-        row.classList.add("row");
-        this.grid.append(row);
-        return row;
     }
 
     addPost(post) {
@@ -17,21 +9,20 @@ class PhotoGrid {
         // add image to row
         const img = document.createElement("img");
         img.loading = "lazy";
-        img.width = post.thumbnail.width;
-        img.height = post.thumbnail.height;
+        img.width = post.thumbnail.width * 0.6;
+        img.height = post.thumbnail.height * 0.6;
+        img.heightPerWidth = img.height / img.width;
         img.src = post.thumbnail.url;
-        this.curRow.append(img);
+        img.style.flexGrow = post.thumbnail.width; // ridiculous hack
+        this.grid.append(img);
 
-        // detect if the row is full if the element's width is lower than its full width
-        // if it's full, adjust height for members of the row to preserve aspect ratio
-        if(img.width < post.thumbnail.width) {
-            const height = Math.round(post.thumbnail.height * img.width / post.thumbnail.width) + "px";
-            this.curRow.querySelectorAll("img").forEach(img => {
-                img.style.height = height;
-            });
-            this.curRow = this.newRow();
-        }
+    }
 
+    fixAspectRatios() {
+        this.grid.querySelectorAll("img").forEach(image => {
+            console.log(image.width, image.height);
+            image.style.height = Math.round(image.width * image.heightPerWidth) + "px";
+        });
     }
 
     onPostsLoaded(posts) {
