1191436924be3f72d2beaafb04a380e24743d6f4
adrian154 <94568a@gmail.com>
2022-03-06 19:27:06 -0800

    photogrid

--- src/process-upload.js
+++ src/process-upload.js
@@ -6,7 +6,7 @@ const sharp = require("sharp");
 // this fixes issues with failing to delete temp files on Windows
 sharp.cache(false);
 
-const THUMBNAIL_HEIGHT = 240;
+const THUMBNAIL_HEIGHT = 320;
 
 const processAsImage = async (filepath, tags) => {
 
@@ -16,8 +16,8 @@ const processAsImage = async (filepath, tags) => {
     // pass the original through sharp to strip metadata
     const versions = {};
     versions.original = {stream: image, contentType: "image/" + meta.format}; // FIXME: meta.format is not guaranteed to be a MIME type!
-    
-    const width = Math.round(meta.width * THUMBNAIL_HEIGHT / meta.height);
+
+    const width = Math.round(meta.width * THUMBNAIL_HEIGHT / (meta.pageHeight || meta.height));
     versions.thumbnail = {stream: image.clone().resize({width, height: THUMBNAIL_HEIGHT, fit: "cover"}).webp(), contentType: "image/webp"};
     versions.thumbnail.width = width;
     versions.thumbnail.height = THUMBNAIL_HEIGHT;
