a6ecd3440ab8be431c807d20253ceba9deee0790
adrian154 <94568a@gmail.com>
2022-03-26 18:29:05 -0700

    hooray, managed to make photogrid JS-less!

--- static/scripts/photogrid.js
+++ static/scripts/photogrid.js
@@ -2,39 +2,31 @@ const SCALE = 0.5;
 
 class PhotoGrid {
 
-    // FIXME: resize not debounced; but is it really important? more at 10!!
     constructor() {
         this.grid = document.getElementById("photogrid");
         this.placeholder = document.getElementById("photogrid-placeholder");
-        window.addEventListener("resize", () => this.fixAspectRatios());
     }
 
-    addPost(post, bulk) {
+    addPost(post) {
 
-        // add image to row
+        // create container
+        const container = document.createElement("div");
+        container.classList.add("photogrid-item");
+        container.style.flexBasis = post.preview.width * SCALE + "px";
+        container.style.flexGrow = post.preview.width;
+        container.aspectRatio = post.preview.height / post.preview.width;
+        this.grid.insertBefore(container, this.placeholder);
+
+        // add image
         const img = document.createElement("img");
         img.classList.add("clickable");
         img.loading = "lazy";
-        img.width = post.preview.width * SCALE;
-        img.height = post.preview.height * SCALE;
-        img.heightPerWidth = img.height / img.width;
         img.src = post.preview.url;
-        img.style.flexGrow = post.preview.width;
-        this.grid.insertBefore(img, this.placeholder);
+        container.append(img);
 
         const index = slideshow.addPost(post);
         img.addEventListener("click", () => slideshow.goto(index));
 
-        if(!bulk) {
-            this.fixAspectRatios();
-        }
-
-    }
-
-    fixAspectRatios() {
-        this.grid.querySelectorAll("img").forEach(image => {
-            image.style.height = Math.round(image.width * image.heightPerWidth) + "px";
-        });
     }
 
     onPostsLoaded(posts) {
@@ -43,7 +35,6 @@ class PhotoGrid {
             post.index = i;
             this.addPost(post, true);
         }
-        this.fixAspectRatios();
     }
 
 }
